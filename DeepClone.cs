41387d5ef43387a60b7ea8db24cdf534ed4db348
Cody Ng <cng@vitralogy.com>
2021-02-03 14:53:27 -0500

    Initial commit

---
+++ DeepClone.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepClone
+{
+    static class DeepClone
+    {
+        static public Object Clone(object input)
+        {
+            if (input == null)
+                return null;
+
+            var inputType = input.GetType();
+            Console.WriteLine($"input={input}, Type={inputType}");
+
+            // if value type, return 
+            if (inputType.IsValueType)
+            {
+                return CloneValueType(input);
+            }
+
+
+            return CloneReferenceType(input, inputType);
+        }
+
+        #region helpers
+        static Object CloneReferenceType(object input, Type inputType)
+        {
+            Console.WriteLine($"Cloning reference type {inputType.Name}...");
+
+            if (inputType == typeof(string))
+            {
+                return CloneString(input);
+            }
+
+            return input;
+        }
+
+        static String CloneString(object obj)
+        {
+            return new string((string)obj);
+        }
+
+        static Object CloneValueType(object obj)
+        {
+            Console.WriteLine("Cloning value type...");
+            //var temp = Convert.ChangeType(input, inputType);
+            return obj;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DeepClone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int input = 1;
+            string input = "abc";
+
+            var result = DeepClone.Clone(input);
+            Console.WriteLine($"result = {result}");
+
+            Console.WriteLine($"is clone referenceEqual() to input? = {object.ReferenceEquals(input, result)}");
+            
+        }
+    }
+}
